9accc5c8860bb267ace6c2d8e1a9b3cd8f84e192
denismarchesan <denismarchesan347@gmail.com>
2025-11-17 04:22:07 +0100

    Casetta rinnovata

---
+++ app-files/data.js
@@ -0,0 +1,723 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20251116_195627_00_012",
+      "name": "IMG_20251116_195627_00_012",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.9013391006126383,
+        "pitch": -0.053013811974189196,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0757149030398363,
+          "pitch": 0.1001508055597693,
+          "rotation": 0,
+          "target": "1-img_20251116_193356_00_001"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0634256989365536,
+          "pitch": -0.007198239112685911,
+          "title": "Soggiorno",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-img_20251116_193356_00_001",
+      "name": "IMG_20251116_193356_00_001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.265705554952026,
+        "pitch": 0.14866490913524721,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6890068498883082,
+          "pitch": 0.14115826914978769,
+          "rotation": 0,
+          "target": "0-img_20251116_195627_00_012"
+        },
+        {
+          "yaw": 2.776214770262288,
+          "pitch": 0.15097765740717684,
+          "rotation": 0,
+          "target": "2-img_20251116_193557_00_002"
+        },
+        {
+          "yaw": -2.263568667806048,
+          "pitch": 0.12237528683777299,
+          "rotation": 0,
+          "target": "3-img_20251116_193922_00_004"
+        },
+        {
+          "yaw": -1.30895900567306,
+          "pitch": 0.07084147766138038,
+          "rotation": 0,
+          "target": "5-img_20251116_194240_00_006"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.8486314669147266,
+          "pitch": 0.034495479406592366,
+          "title": "Cucina con camino",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.3085203972723427,
+          "pitch": -0.043377750602463294,
+          "title": "Vano scala",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.2086340044305963,
+          "pitch": -0.008683244556154435,
+          "title": "Sottoscala e servizio piano terra",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.6435476530861193,
+          "pitch": -0.04236530703332875,
+          "title": "Uscita",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-img_20251116_193557_00_002",
+      "name": "IMG_20251116_193557_00_002",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 3.0837085634419257,
+        "pitch": 0.20186049036373355,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16343638697869167,
+          "pitch": 0.2294188377144728,
+          "rotation": 0,
+          "target": "1-img_20251116_193356_00_001"
+        },
+        {
+          "yaw": -1.4287033548413213,
+          "pitch": 0.06567339458418608,
+          "rotation": 0,
+          "target": "3-img_20251116_193922_00_004"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.23278261368637843,
+          "pitch": 0.10321004625477848,
+          "title": "Soggiorno",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.4073499530567588,
+          "pitch": -0.06979419948600096,
+          "title": "Sottoscala e servizio",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-img_20251116_193922_00_004",
+      "name": "IMG_20251116_193922_00_004",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.145256338631116,
+        "pitch": 0.3000519658118517,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1065364845090766,
+          "pitch": 0.2841519430968713,
+          "rotation": 0,
+          "target": "4-img_20251116_194121_00_005"
+        },
+        {
+          "yaw": -0.48706972332294995,
+          "pitch": 0.018340410714539956,
+          "rotation": 0,
+          "target": "5-img_20251116_194240_00_006"
+        },
+        {
+          "yaw": 1.2269452739346747,
+          "pitch": 0.0783832241993423,
+          "rotation": 0,
+          "target": "2-img_20251116_193557_00_002"
+        },
+        {
+          "yaw": 0.6779328460137748,
+          "pitch": 0.08315002121630499,
+          "rotation": 0,
+          "target": "0-img_20251116_195627_00_012"
+        },
+        {
+          "yaw": 0.2905758284858848,
+          "pitch": 0.24048600968933087,
+          "rotation": 0,
+          "target": "1-img_20251116_193356_00_001"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.961945455464594,
+          "pitch": 0.15106869380845822,
+          "title": "Servizio piano terra",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.5134903545601048,
+          "pitch": -0.08579523120701893,
+          "title": "vano scala",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.6478498349275164,
+          "pitch": -0.028510133523894865,
+          "title": "Uscita",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.2741716415408302,
+          "pitch": -0.011731648096372282,
+          "title": "Cucina con camino",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.17146728011615586,
+          "pitch": 0.11901611442409177,
+          "title": "Soggiorno",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-img_20251116_194121_00_005",
+      "name": "IMG_20251116_194121_00_005",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.121536605106108,
+        "pitch": 0.2718489519650511,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8278844142884054,
+          "pitch": 0.05653239943679367,
+          "rotation": 0,
+          "target": "3-img_20251116_193922_00_004"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-img_20251116_194240_00_006",
+      "name": "IMG_20251116_194240_00_006",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.9443942491491626,
+        "pitch": -0.08057147952751542,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.960111176300515,
+          "pitch": 0.475954802793364,
+          "rotation": 0,
+          "target": "1-img_20251116_193356_00_001"
+        },
+        {
+          "yaw": 3.098472181951715,
+          "pitch": -0.356051198752116,
+          "rotation": 0,
+          "target": "6-img_20251116_194537_00_007"
+        },
+        {
+          "yaw": 1.7691314962600604,
+          "pitch": 0.17391907818711516,
+          "rotation": 0,
+          "target": "0-img_20251116_195627_00_012"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8579512802532516,
+          "pitch": 0.4479441036819072,
+          "title": "Soggiorno",
+          "text": "Text"
+        },
+        {
+          "yaw": 3.0608407289390165,
+          "pitch": -0.4715786657791661,
+          "title": "Vano scala superiore",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.759075539165428,
+          "pitch": 0.08939508033082078,
+          "title": "Uscita",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-img_20251116_194537_00_007",
+      "name": "IMG_20251116_194537_00_007",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.3463306723799917,
+        "pitch": 0.30682469962339454,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.29274414600600274,
+          "pitch": 0.6502161131474509,
+          "rotation": 0,
+          "target": "5-img_20251116_194240_00_006"
+        },
+        {
+          "yaw": 2.485005366909377,
+          "pitch": 0.39861625642428855,
+          "rotation": 0,
+          "target": "7-img_20251116_194650_00_008"
+        },
+        {
+          "yaw": 1.107918638906602,
+          "pitch": 0.29546478069296533,
+          "rotation": 0,
+          "target": "10-img_20251116_195136_00_011"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.298170887318893,
+          "pitch": 0.3454922622918559,
+          "title": "Disimpegno primo piano",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.0485749102925688,
+          "pitch": 0.13865237644892048,
+          "title": "Stanza Matrimoniale",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.1952166020493138,
+          "pitch": 0.5513481553451953,
+          "title": "Vano scala inferiore",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-img_20251116_194650_00_008",
+      "name": "IMG_20251116_194650_00_008",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.637631696350569,
+        "pitch": 0.5539495967633385,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.25593600809203565,
+          "pitch": 0.34997502755976484,
+          "rotation": 0,
+          "target": "8-img_20251116_194836_00_0091"
+        },
+        {
+          "yaw": -1.6041773515620363,
+          "pitch": 0.27309213790081976,
+          "rotation": 0,
+          "target": "9-img_20251116_194942_00_010"
+        },
+        {
+          "yaw": 2.4796675126160235,
+          "pitch": 0.49942548472838055,
+          "rotation": 0,
+          "target": "6-img_20251116_194537_00_007"
+        },
+        {
+          "yaw": 3.0138095924730752,
+          "pitch": 0.2236219192526825,
+          "rotation": 0,
+          "target": "10-img_20251116_195136_00_011"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.009066757698635,
+          "pitch": 0.014393423877782752,
+          "title": "stanza matrimoniale",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.5248796254028996,
+          "pitch": 0.1319657883479124,
+          "title": "Stanza singola",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.13070376972479103,
+          "pitch": 0.17630694160877525,
+          "title": "servizio primo piano",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.370119235331881,
+          "pitch": 0.40590693633680885,
+          "title": "Vano scala",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-img_20251116_194836_00_0091",
+      "name": "IMG_20251116_194836_00_009(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.2427657244891375,
+        "pitch": 0.34709526777039557,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5672273108280645,
+          "pitch": 0.3698043537220155,
+          "rotation": 0,
+          "target": "7-img_20251116_194650_00_008"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.4827531801278866,
+          "pitch": 0.2712554060567456,
+          "title": "Disimpegno primo piano",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "9-img_20251116_194942_00_010",
+      "name": "IMG_20251116_194942_00_010",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1217156300922086,
+          "pitch": 0.17959286388780882,
+          "rotation": 0,
+          "target": "7-img_20251116_194650_00_008"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.095786652507213,
+          "pitch": 0.04987849750849804,
+          "title": "Disimpegno primo piano",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "10-img_20251116_195136_00_011",
+      "name": "IMG_20251116_195136_00_011",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.5241304367367903,
+        "pitch": 0.2117751245400843,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.071862638763786,
+          "pitch": 0.18470963060999424,
+          "rotation": 0,
+          "target": "7-img_20251116_194650_00_008"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.115815720437336,
+          "pitch": 0.04758479699493812,
+          "title": "Disimpegno primo piano",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
